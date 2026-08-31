4b2a465ca156c74796663b57a22f6e920aaf7605
gmdott <gustavo.moura-dev@outlook.com>
2026-08-31 02:03:51 -0300

    add funcao de graficos e escolha do user

--- main.py
+++ main.py
@@ -6,15 +6,36 @@ csv = 'clean_final_data.csv'
 def graphChoose():
 
     os.system('clear')
-    chooseGraph_User = input("Choose wich graphic do you want to display the info")
-
     print('[1] - Plot\n[2] - Scatter\n[3] - Bar\n[4] - Stemn\n[5] - Fill Between\n[6] - Stackplot')
 
-    
+    chooseGraph_User = input("Choose wich graphic do you want to display the info: ")
 
+    if chooseGraph_User == '1':
+        os.system('clear')
+        print('You choose the Plot graph and want to show just', chooseUser, '\n')
+
+    elif chooseGraph_User == '2':
+        os.system('clear')
+        print('You choose the Scatter graph and want to show just', chooseUser, '\n')
+
+    elif chooseGraph_User == '3':
+        os.system('clear')
+        print('You choose the Bar graph and want to show just', chooseUser, '\n')
+
+    elif chooseGraph_User == '4':
+            os.system('clear')
+            print('You choose the Stemn graph and want to show just', chooseUser, '\n')
+
+    elif chooseGraph_User == '5':
+            os.system('clear')
+            print('You choose the Fill Between graph and want to show just', chooseUser, '\n')
+
+    elif chooseGraph_User == '6':
+            os.system('clear')
+            print('You choose the Stackplot graph and want to show just', chooseUser, '\n')
     return 0
 
-
+os.system('clear')
 print("\n[1] - Order Date\n[2] - Quantity\n[3] - Product Name\n[4] - Unit Price\n[5] - City\n[6] - Age")
 
 chooseUser = input("\nChoose wich column you want to see: ")
@@ -24,9 +45,12 @@ if chooseUser == '1':
         "ProductName",
         "OrderDate"], nrows=20)
 
+    chooseUser = "Oder Date"
+
     #os.system está obsoleto, ele está sendo utilizado apenas como exemplo
     os.system('clear')
-    print('\n', df,'\n')
+    graphChoose()
+    #print('\n', df,'\n')
 
 elif chooseUser == '2':
     df = pd.read_csv(csv, usecols=[
@@ -35,13 +59,19 @@ elif chooseUser == '2':
     #Transforma o os valores de quantity que estão em float, em int e também verifica se tem valor NaN
     df["Quantity"] = df["Quantity"].fillna(0).astype(int)
 
+    chooseUser = "Quantity"
+
     os.system('clear')
-    print('\n', df,'\n')
+    graphChoose()
+    #print('\n', df,'\n')
 
 elif chooseUser == '3':
     df = pd.read_csv(csv, usecols=[
         "ProductName"
     ], nrows=20)
+
+    chooseUser = "Product Name"
+
 
     os.system('clear')
     print('\n', df,'\n')
@@ -52,8 +82,11 @@ elif chooseUser == '4':
         "UniPrice",
     ], nrows=20)
 
+    chooseUser = "Unit Price"
+
     os.system('clear')
-    print('\n', df,'\n')
+    graphChoose()
+    #print('\n', df,'\n')
 
 elif chooseUser == '5':
     df = pd.read_csv(csv, usecols=[
@@ -61,8 +94,11 @@ elif chooseUser == '5':
         "City"  
     ], nrows=20)
 
+    chooseUser = "City"
+
     os.system('clear')
-    print('\n', df,'\n')
+    graphChoose()
+    #print('\n', df,'\n')
 
 elif chooseUser == '6':
     df = pd. read_csv(csv, usecols= [
@@ -70,5 +106,8 @@ elif chooseUser == '6':
         "Age"
     ], nrows=20)
 
+    chooseUser = "Age"
+
     os.system('clear')
-    print('\n', df,'\n')
+    graphChoose()
+    #print('\n', df,'\n')
